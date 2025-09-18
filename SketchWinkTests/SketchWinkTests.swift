f1089f3f09eeeba7e8822e5942487f841742884b
Alejandro <alejandroo14@gmail.com>
2025-09-18 13:13:03 -0500

    Initial Commit

---
+++ SketchWinkTests/SketchWinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  SketchWinkTests.swift
+//  SketchWinkTests
+//
+//  Created by alejandro on 18/09/25.
+//
+
+import Testing
+@testable import SketchWink
+
+struct SketchWinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
